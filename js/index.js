295fec84e6295c50084a10cebba5b98a17923b48
Do Trung Hoa <jvevermore95@gmail.com>
2021-05-07 09:12:07 +0700

    auto click

--- js/index.js
+++ js/index.js
@@ -4,17 +4,27 @@ storage = chrome.storage;
 if (location.hostname == "wsm.zinza.com.vn") {
   today = current_date.getDate();
   index = today - 1;
-  window.onload = function() {
+  window.onload = function () {
     today_cell = $("td.current-month:not(.past)").first();
     times = $(today_cell).find(".checktime").children().length;
     if (times == 0) {
       cf_check_in = confirm("Hôm nay chưa checkin. Bạn muốn checkin luôn không?");
       if (cf_check_in) {
-        storage.local.set({checked_in: true}, function(){});
-        storage.local.set({last_checked_in_day: current_date.getDate()}, function(){});
+        if ($(".float-right > a").length > 0) {
+          $.ajax({
+            type: "POST",
+            url: "https://wsm.zinza.com.vn/user/working_times",
+            success: function() {
+              alert("success");
+            }
+          });
+
+          storage.local.set({ checked_in: true }, function () { });
+          storage.local.set({ last_checked_in_day: current_date.getDate() }, function () { });
+        }
       }
     }
-    else if (times == 1){
+    else if (times == 1) {
       time_check_in = $(today_cell).find(".checktime").children().first().text();
       hour_check_in = time_check_in.split(":")[0];
       minute_check_in = time_check_in.split(":")[1];
@@ -24,44 +34,72 @@ if (location.hostname == "wsm.zinza.com.vn") {
       timediff = (current_date - format_time_check_in) / 3600000;
 
       if (timediff > 9) {
-        confirm("Hôm nay chưa checkout");
+        cf_checkout = confirm("Hôm nay chưa checkout. Bạn có muốn checkout luôn không?");
+        if (cf_checkout) {
+          if ($(".float-right > a").length > 0) {
+            href = $(".float-right > a")[0].href;
+            $.ajax({
+              type: "POST",
+              url: href,
+              success: function() {
+                alert("success");
+              }
+            });
+
+            storage.local.set({ checked_out: true }, function () { });
+            storage.local.set({ last_checked_out_day: current_date.getDate() }, function () { });
+          }
+        }
       }
     }
     else {
-      storage.sync.set({last_checked_out_day: today});
-      storage.sync.set({checked_out: true});
+      storage.local.set({ checked_out: true }, function () { });
+      storage.local.set({ last_checked_out_day: current_date.getDate() }, function () { });
     }
   };
 }
 else {
   homnaylathumay = current_date.getDay();
-  if (![0,6].includes(homnaylathumay)) {
+  if (![0, 6].includes(homnaylathumay)) {
     //kiem tra checkin
-    storage.local.get(null, function(data) {
+    storage.local.get(null, function (data) {
       var checked_in = data.checked_in;
       var last_checked_in_day = data.last_checked_in_day;
 
       console.log("checked_in", checked_in);
       console.log("last_checked_in_day", last_checked_in_day);
 
-      if (last_checked_in_day != current_date.getDate()) {
-        if (checked_in != false) {
-          checkin_confirm = confirm("Hôm nay bạn đã check in chưa?");
-          if (checkin_confirm) {
-            storage.local.set({checked_in: true}, function(){});
-            storage.local.set({last_checked_in_day: current_date.getDate()}, function(){});
-          }
+      if (last_checked_in_day == current_date.getDate() && checked_in) {
+        //thank god. i dont have to do anything here
+      }
+      else {
+        checkin_confirm = confirm("Hôm nay bạn đã check in chưa?");
+        if (checkin_confirm) {
+          storage.local.set({ checked_in: true }, function () { });
+          storage.local.set({ last_checked_in_day: current_date.getDate() }, function () { });
         }
       }
     });
 
     //kiem tra checkout
     if (current_date.getHours() >= 18) {
-      checkout_confirm = confirm("Hôm nay bạn đã check out chưa?");
-      if (checkin_confirm) {
-        storage.local.set({checked_out: true}, function(){});
-        storage.local.set({last_checked_out_day: current_date.getDate()}, function(){});
-      }
+      storage.local.get(null, function (data) {
+        var checked_out = data.checked_out;
+        var last_checked_out_day = data.last_checked_out_day;
+
+        console.log("checked_out", checked_out);
+        console.log("last_checked_out_day", last_checked_out_day);
+
+        if (last_checked_out_day != current_date.getDate()) {
+          if (checked_out != false) {
+            checkin_confirm = confirm("Hôm nay bạn đã check in chưa?");
+            if (checkin_confirm) {
+              storage.local.set({ checked_in: true }, function () { });
+              storage.local.set({ last_checked_in_day: current_date.getDate() }, function () { });
+            }
+          }
+        }
+      });
     }
   }
 }
