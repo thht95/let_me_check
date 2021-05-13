2f465c5790c296afaf4d05e66d80dd75bf633133
Do Trung Hoa <jvevermore95@gmail.com>
2021-05-13 09:41:39 +0700

    add 1s before check

--- js/index.js
+++ js/index.js
@@ -5,63 +5,65 @@ if (location.hostname == "wsm.zinza.com.vn") {
   today = current_date.getDate();
   index = today - 1;
   $(document).ready(function () {
-    today_cell = $("td.current-month:not(.past)").first();
-    times = $(today_cell).find(".checktime").children().length;
-    if (times == 0) {
-      cf_check_in = confirm("Hôm nay chưa checkin. Bạn muốn checkin luôn không?");
-      if (cf_check_in) {
-        if ($(".float-right > a").length > 0) {
-          $.ajax({
-            type: "POST",
-            url: "https://wsm.zinza.com.vn/user/working_times",
-            success: function () {
-              alert("Checkin success. Reload browser pls!");
-            },
-            error: function () {
-              alert("Something went wrong. Pls checkin manually");
-            }
-          });
-
-          storage.local.set({ checked_in: true }, function () { });
-          storage.local.set({ last_checked_in_day: current_date.getDate() }, function () { });
-        }
-      }
-    }
-    else if (times == 1) {
-      time_check_in = $(today_cell).find(".checktime").children().first().text();
-      hour_check_in = time_check_in.split(":")[0];
-      minute_check_in = time_check_in.split(":")[1];
-      format_time_check_in = new Date();
-      format_time_check_in.setMinutes(minute_check_in);
-      format_time_check_in.setHours(hour_check_in);
-      timediff = (current_date - format_time_check_in) / 3600000;
-
-      if (timediff > 9) {
-        cf_checkout = confirm("Hôm nay chưa checkout. Bạn có muốn checkout luôn không?");
-        if (cf_checkout) {
+    setTimeout(function() {
+      today_cell = $("td.current-month:not(.past)").first();
+      times = $(today_cell).find(".checktime").children().length;
+      if (times == 0) {
+        cf_check_in = confirm("Hôm nay chưa checkin. Bạn muốn checkin luôn không?");
+        if (cf_check_in) {
           if ($(".float-right > a").length > 0) {
-            href = $(".float-right > a")[0].href;
             $.ajax({
-              type: "PATCH",
-              url: href,
+              type: "POST",
+              url: "https://wsm.zinza.com.vn/user/working_times",
               success: function () {
-                alert("Checkout success. Reload browser pls!");
+                alert("Checkin success. Reload browser pls!");
               },
               error: function () {
-                alert("Something went wrong. Pls checkout manually");
+                alert("Something went wrong. Pls checkin manually");
               }
             });
 
-            storage.local.set({ checked_out: true }, function () { });
-            storage.local.set({ last_checked_out_day: current_date.getDate() }, function () { });
+            storage.local.set({ checked_in: true }, function () { });
+            storage.local.set({ last_checked_in_day: current_date.getDate() }, function () { });
           }
         }
       }
-    }
-    else {
-      storage.local.set({ checked_out: true }, function () { });
-      storage.local.set({ last_checked_out_day: current_date.getDate() }, function () { });
-    }
+      else if (times == 1) {
+        time_check_in = $(today_cell).find(".checktime").children().first().text();
+        hour_check_in = time_check_in.split(":")[0];
+        minute_check_in = time_check_in.split(":")[1];
+        format_time_check_in = new Date();
+        format_time_check_in.setMinutes(minute_check_in);
+        format_time_check_in.setHours(hour_check_in);
+        timediff = (current_date - format_time_check_in) / 3600000;
+
+        if (timediff > 9) {
+          cf_checkout = confirm("Hôm nay chưa checkout. Bạn có muốn checkout luôn không?");
+          if (cf_checkout) {
+            if ($(".float-right > a").length > 0) {
+              href = $(".float-right > a")[0].href;
+              $.ajax({
+                type: "PATCH",
+                url: href,
+                success: function () {
+                  alert("Checkout success. Reload browser pls!");
+                },
+                error: function () {
+                  alert("Something went wrong. Pls checkout manually");
+                }
+              });
+
+              storage.local.set({ checked_out: true }, function () { });
+              storage.local.set({ last_checked_out_day: current_date.getDate() }, function () { });
+            }
+          }
+        }
+      }
+      else {
+        storage.local.set({ checked_out: true }, function () { });
+        storage.local.set({ last_checked_out_day: current_date.getDate() }, function () { });
+      }
+    }, 1000);
   });
 }
 else {
@@ -74,7 +76,6 @@ else {
 
       console.log("checked_in", checked_in);
       console.log("last_checked_in_day", last_checked_in_day);
-      console.log(last_checked_in_day == current_date.getDate());
 
       if (last_checked_in_day == current_date.getDate() && checked_in) {
         //thank god. i dont have to do anything here
